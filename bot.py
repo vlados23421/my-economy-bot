96c72a6298fc59c925e50f84345c0c85f8310180
vlados23421 <vladosgaros098@gmail.com>
2026-07-26 22:15:47 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from datetime import datetime, timedelta
 
 # --- 1. ИНИЦИАЛИЗАЦИЯ БОТА И ДАННЫХ SUPABASE ---
 # На FPS.ms мы пропишем эти токены прямо в панели управления
-TOKEN = os.environ.get('8957594048:AAG0K_PyHd-vCebdH-BCiD-oMYBegtt3TXE')
+TOKEN = os.environ.get('8957594048:AAFmdWyLWYxDNjE7tdw1xBfZVYjAK7Qjnhs')
 ADMIN_ID = 8915047087  # !!! ЗАМЕНИТЕ НА ВАШ REAL TELEGRAM ID !!!
 
 bot = telebot.TeleBot(TOKEN)
